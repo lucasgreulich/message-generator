1994cff540d61b5ccfcebfa7d62787707258d889
Lucas Greulich <lucas.g.greulich@gmail.com>
2023-10-03 16:56:16 -0700

    message array created

---
+++ script.js
@@ -0,0 +1,5 @@
+const messageWords = [
+    'Calm', 'Gentle', 'Mellow', 'Quiet', 'Relaxing', 'Tranquil', 'Soft', 'Delicate', 'Light',
+    'Hypnotic', 'Dreamy', 'Balmy', 'Salving', 'Comfort', 'Cheering'
+];
+console.log(messageWords);
